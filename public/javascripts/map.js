84a74ab7fe27c1931c84f66c29ebf2f16ffcbb9a
Andres Erira <haeh2007@gmail.com>
2020-11-13 18:22:06 -0500

    Se instala leafter para los mapas se configuran y harcodean 3 marcadores se personaliza un poco el index de el tema grayscale

---
+++ public/javascripts/map.js
@@ -0,0 +1,9 @@
+var mymap = L.map('main_map').setView([3.4282738, -76.4700004,17], 13);
+
+L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    attribution: '&copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors'
+}).addTo(mymap);
+
+L.marker([3.4282738, -76.4700004,17]).addTo(mymap);
+L.marker([3.4329001,-76.4678761]).addTo(mymap);
+L.marker([3.4282738, -76.4678761,17]).addTo(mymap);
